4e00b777acef00d05054df90f7b668e8e7f9c7f2
Alain Tran <alaint77@gmail.com>
2019-09-13 11:35:18 +0200

    Add files via upload

---
+++ build/precache-manifest.a015364be6988c14edee948cfd74d14e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502161b0302f410bc643aa8a1fff1543",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bf682554e27a998a949",
+    "url": "/static/css/main.bec1dcbd.chunk.css"
+  },
+  {
+    "revision": "c92ecfaa8d3c9c06d282",
+    "url": "/static/js/2.6bcb67b5.chunk.js"
+  },
+  {
+    "revision": "0bf682554e27a998a949",
+    "url": "/static/js/main.7db5bcb8.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
